172567afcf190c7a034b1add662fd47a22061a87
riyajath-ahamed <riyajatha@gmail.com>
2024-09-24 21:34:08 +0530

    etl

---
+++ Internal/etl/elt.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	// "database/sql"
+// 	// "fmt"
+// 	// "log"
+// )
+
+// // Extract function to get data
+// func Extract() []string {
+
+// }
+
+// // Transform function to process data
+// func Transform(data []string) []string {
+
+// }
+
+// // Load function to store data in the database
+// func Load(data []string) {
+
+// }
+
+func main() {
+
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+#RestAPI Go Lang
+
+This is a simple REST API written in Go Lang. It uses the Gin and stores data in memory.
+
+- [] [Sample Git Repo](https://github.com/qiangxue/go-rest-api/blob/master/cmd/server/main.go)
+
